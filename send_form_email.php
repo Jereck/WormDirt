de36cf509baef08e363fdf7c432da7bb5452102c
Jake Reck <jakereck@gmail.com>
2017-09-16 20:42:20 -0700

    Added better email functionality to the contact form

--- send_form_email.php
+++ send_form_email.php
@@ -1,18 +1,21 @@
 <?php
 if(isset($_POST['submit'])){
-    $to = "wormdirtdevelopment@gmail.com"; // this is your Email address
+    $to = "wormdirt@wormdirtdevelopment.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
-    $subject = "Form submission";
+    $subject = "Email from WormDirt Development";
     $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
 
 
-    $headers = "From:" . $from;
-    $headers .= "Reply-To:" . $from;
-    $headers .= "CC: jakereck@gmail.com\r\n"
+    $headers = "From:" . $from . "\r\n";
+    $headers .= "Reply-To:" . $from . "\r\n";
+    $headers .= "Organization: WormDirt Development\r\n";
+    $headers .= "X-Mailer: PHP" . phpversion() . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
 
-    mail($to,$subject,$message,$headers);
+    mail($to, $subject, $message, $headers);
 
     header("Location: http://wormdirtdevelopment.com/redirect.html");
     echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";

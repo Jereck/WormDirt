b5d0b98f6b79b3cab8dc60337854b735b581f004
Jake Reck <jakereck@gmail.com>
2017-09-16 19:20:26 -0700

    Added some functionality fixes to contact form

--- send_form_email.php
+++ send_form_email.php
@@ -5,14 +5,15 @@ if(isset($_POST['submit'])){
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
     $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
 
     $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
+    $headers .= "Reply-To:" . $from;
+    $headers .= "CC: jakereck@gmail.com\r\n"
+
     mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2);
+
     header("Location: http://wormdirtdevelopment.com/redirect.html");
     echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
     }
